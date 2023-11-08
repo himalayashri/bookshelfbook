a7656d8b61099db7a614d793aaa24752492312aa
Himalaya shri Namdev <himalayaofficials@gmail.com>
2023-11-08 21:58:02 +0530

    cors added

--- index.js
+++ index.js
@@ -8,13 +8,22 @@ const Home = require("./Routes/HomeRoute")
 mongoDB();
 
 // app.use((req, res, next) => {
-//     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+//     res.header("Access-Control-Allow-Credentials", "true")
+//     res.header("Access-Control-Request-Method", "POST")
 //     res.header(
 //         "Access-Control-Allow-Headers",
 //         "Origin, X-requested-With, Content-Type, Accept"
 //     );
 //     next();
 // })
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 app.use(express.json());
 
